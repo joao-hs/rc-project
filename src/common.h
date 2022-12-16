3bb027ae0d58abf883296fc16b6c90cc8b025a9a
JS-GHub <joaohsereno@tecnico.ulisboa.pt>
2022-12-16 02:08:54 +0000

    Reorganize, include guard and debug

--- src/common.h
+++ src/common.h
@@ -1,7 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+
+#ifndef COMMON_H
+#define COMMON_H
+
 #define TRUE 1
 #define FALSE 0
 #define CMD_ID_LEN 3
-#define STATUS_LEN 3
+#define STATUS_LEN 6
 #define PLID_LEN 6
 #define WORD_MIN 3
 #define WORD_MAX 30
@@ -16,17 +30,12 @@
 #define MAX_UDP_RESPONSE CMD_ID_LEN + 1 + STATUS_LEN + 1 + 2 + 2 + WORD_MAX*2 
 #define MAX_TCP_RESPONSE CMD_ID_LEN + 1 + STATUS_LEN + 1 + FNAME_LEN + 1 + FSIZE_LEN + 1 + FDATA_MAX + 1
 
-
 #define MAX(x, y) ({__typeof__ (x) _x = (x); __typeof__ (y) _y = (y); _x > _y ? _x : _y;})
 #define MIN(x, y) ({__typeof__ (x) _x = (x); __typeof__ (y) _y = (y); _x <= _y ? _x : _y;})
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <signal.h>
+typedef struct file_info {
+    char * f_name;
+    ssize_t f_size;
+    int * f_data;
+} File;
+#endif /* COMMON_H */
