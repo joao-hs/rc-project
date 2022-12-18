b983b49228f85f5854a2e8559143ca1faf08b77d
JS-GHub <joaohsereno@tecnico.ulisboa.pt>
2022-12-18 17:18:26 +0000

    ifndef

--- src/common.h
+++ src/common.h
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #ifndef COMMON_H
 #define COMMON_H
@@ -27,15 +28,16 @@
 #define MAX_HOST 253
 #define MAX_PORT 6
 #define MAX_MESSAGE CMD_ID_LEN + 1 + WORD_MAX + 1
-#define MAX_UDP_RESPONSE CMD_ID_LEN + 1 + STATUS_LEN + 1 + 2 + 2 + WORD_MAX*2 
+#define MAX_UDP_RESPONSE CMD_ID_LEN + 1 + STATUS_LEN + 1 + 2 + 2 + WORD_MAX * 2
 #define MAX_TCP_RESPONSE CMD_ID_LEN + 1 + STATUS_LEN + 1 + FNAME_LEN + 1 + FSIZE_LEN + 1 + FDATA_MAX + 1
 
-#define MAX(x, y) ({__typeof__ (x) _x = (x); __typeof__ (y) _y = (y); _x > _y ? _x : _y;})
-#define MIN(x, y) ({__typeof__ (x) _x = (x); __typeof__ (y) _y = (y); _x <= _y ? _x : _y;})
+#define MAX(x, y) ({__typeof__ (x) _x = (x); __typeof__ (y) _y = (y); _x > _y ? _x : _y; })
+#define MIN(x, y) ({__typeof__ (x) _x = (x); __typeof__ (y) _y = (y); _x <= _y ? _x : _y; })
 
 typedef struct file_info {
-    char * f_name;
+    FILE *f;
+    char f_name[FNAME_LEN];
     ssize_t f_size;
-    int * f_data;
-} File;
+    char *f_data;
+} F_INFO;
 #endif /* COMMON_H */
